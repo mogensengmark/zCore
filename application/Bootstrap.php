a856c3568ddd8815270fc8bc4cdb7e55a06457a7
Mogens Engmark <me@rtsweb.dk>
2012-03-09 14:07:41 +0100

    Now pulling menu from database.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -15,8 +15,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
             'basePath'  => dirname(__FILE__),
         ));
         return $autoloader;
-    }	
-	
+    }
+
     /**/
     protected function _initView()
     {
@@ -30,18 +30,18 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
                                            'text/html; charset=utf-8');
         return $view;
     }
-    
-    
+
+
     protected function _initzCoreDB()
     {
     	$this->bootstrap('db');
     	$db = $this->getResource('db');
-    	
+
     	Zend_Registry::set('db', $db);
     }
-    
+
     /**
-    
+
 	protected function _initDocType()
 	{
 		$this->bootstrap('view');
@@ -49,17 +49,24 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		$view->doctype('XHTML1_STRICT');
 	}
 	/**/
-	
-	
+
+
 	protected function _initNavigation()
 	{
 		$this->bootstrap('layout');
 		$layout = $this->getResource('layout');
 		$view = $layout->getView();
-		$config = new Zend_Config_Xml(APPLICATION_PATH . '/configs/navigation.xml', 'nav');
-		
-		$navigation = new Zend_Navigation($config);
-		$view->navigation($navigation); 
+
+		// Loading menu data mapper
+		$menuMapper = new Application_Model_Mapper_Menu();
+		// Get menu value
+		$menuStructure = $menuMapper->getMenu(1);
+/**
+		echo "CONFIG";
+		Zend_Debug::dump($config);
+/**/
+	    // Assigning to view
+		$view->navigation($menuStructure);
 	}
 }
 
