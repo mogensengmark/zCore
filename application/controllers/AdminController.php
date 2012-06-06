fa37aa2ba5205bba626205f1a9a75f99fe4d1278
Mogens Engmark <me@rtsweb.dk>
2012-06-06 10:22:45 +0200

    Comitting prior to moving git to github.

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -2,9 +2,9 @@
 
 /**
  * {0}
- * 
+ *
  * @author
- * @version 
+ * @version
  */
 
 class AdminController extends Zend_Controller_Action
@@ -12,22 +12,26 @@ class AdminController extends Zend_Controller_Action
 	// Default option for forms.
 	private $options = '';
 
-	
+
 	/**
 	 * The default action - show the home page
 	 */
-    public function indexAction() 
+    public function indexAction()
     {
         // TODO Auto-generated {0}::indexAction() default action
+
+
+
+
     }
 
     public function userformAction()
     {
     	$options = '';
     	$form = new Default_Form_userForm($options);
-    	$this->view->form = $form; 
+    	$this->view->form = $form;
     }
-    
-    
+
+
 }
 
--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -4,7 +4,7 @@ require_once 'zCore/zCoreAuthController.php';
 /**
  * AuthController
  * Functionality for authentication of users
- * 
+ *
  * @author Mogens Engmark
  *
  */
@@ -27,27 +27,38 @@ class AuthController extends Zend_Controller_Action
 
         if ($this->getRequest()->isPost()) {
         	//echo "login posted<br>";
-        	
+
             // Get posted data
         	$form_data = $this->getRequest()->getPost();
-            //Zend_Debug::dump($form_data);	
-            
+            //Zend_Debug::dump($form_data);
+
             // Did validation pass??
             if ($form->isValid($form_data)) {
+
+                $zCoreHelper = new zCoreHelper();
+
                 $username = $form->getValue('username');
-                $password = $form->getValue('password');
-                
-                // Calls validation 
-                $auth_adapter = new zCore_Auth_Adapter($username, $password);
+                $password = $zCoreHelper->encodePassword($form->getValue('password'));
+                $password2 = $form->getValue('password');
+
+                // Calls validation
+                $auth_adapter = new zCore_Auth_Adapter($username, $password2);
                 // Using Zend_Auth for authenticating with zCore_Auth_Adapter
                 Zend_Auth::getInstance()->authenticate($auth_adapter);
 
-                
+                $userModelMapper = new Application_Model_Mapper_User();
+
+                $helper = new zCoreHelper();
+
+
+
+                $userModelMapper->validateUser($username, $helper->encodePassword($password));
+
 //                echo "LOGIN VALID<br>";
-                
+
                 /// @todo handle login errors
-                /**/	
-                $this->_redirect('/admin/index');
+                /**/
+                 $this->_redirect('/admin/index');
                 /**/
             }
         }
